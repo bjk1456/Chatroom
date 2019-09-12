e1b72b86b4f39b440dded1d286aaa4238aa1e63e
Benjamin Kelly <kelbenj@Benjamins-MacBook-Air.local>
2019-09-12 14:56:38 -0400

    Create test for: login, chat, and leave.

--- src/main/test/java/edu.udacity.java.nano/edu/udacity/java/nano/WebSocketChatApplicationTest.java
+++ src/main/test/java/edu.udacity.java.nano/edu/udacity/java/nano/WebSocketChatApplicationTest.java
@@ -28,48 +28,65 @@ class WebSocketChatApplicationTest {
 
     @Autowired
     private MockMvc mvc;
-    private String url = "http://localhost:8080/";
-    //WebDriver driver;
     WebDriver driver;
 
-
-    @Before
-    public void launchBrowser() {
-
-
-    }
-    void main() {
-    }
-
     @org.junit.jupiter.api.Test
-    void login() throws Exception {
-        System.setProperty("webdriver.gecko.driver","/Users/benjaminkelly/Downloads/geckodriver");
+    void joinChat() throws Exception {
+        System.setProperty("webdriver.gecko.driver","/Users/kelbenj/Downloads/geckodriver");
         this.driver = new FirefoxDriver();
+        this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         this.driver.get("http://localhost:8080/");
         this.driver.findElement(By.id("username")).sendKeys("Jack");
         this.driver.findElement(By.className("act-but")).click();
-
-        this.driver.findElement(By.cssSelector("body")).sendKeys(Keys.COMMAND +"t");
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.open(\"http://localhost:8080\");");
         ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
         driver.switchTo().window(tabs.get(1));
         this.driver.findElement(By.id("username")).sendKeys("Sam");
         this.driver.findElement(By.className("act-but")).click();
-
-        /**
-        ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
-        driver.switchTo().window(tabs.get(0));
-
-        this.driver.get("http://localhost:8080/");
-        this.driver.findElement(By.id("username")).sendKeys("Same");
-        driver.findElement(By.className("act-but")).click();
-         */
-
         assertEquals("2",driver.findElement(By.className("chat-num")).getText());
+        this.driver.quit();
     }
 
     @org.junit.jupiter.api.Test
-    void index() {
+    void chat() throws Exception {
+        System.setProperty("webdriver.gecko.driver","/Users/kelbenj/Downloads/geckodriver");
+        this.driver = new FirefoxDriver();
+        this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        this.driver.get("http://localhost:8080/");
+        this.driver.findElement(By.id("username")).sendKeys("Ronald");
+        this.driver.findElement(By.className("act-but")).click();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.open(\"http://localhost:8080\");");
+        ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        this.driver.findElement(By.id("username")).sendKeys("Sam");
+        this.driver.findElement(By.className("act-but")).click();
+        this.driver.findElement(By.className("mdui-textfield-input")).sendKeys("Hi Ronald! It's your friend Sam!");
+        this.driver.findElement(By.id("sendMsg")).click();
+        driver.switchTo().window(tabs.get(0));
+        assertEquals("Sam: Hi Ronald! It's your friend Sam!",this.driver.findElement(By.className("mdui-card-content")).getText());
+        this.driver.quit();
+    }
+
+    @org.junit.jupiter.api.Test
+    void leave() {
+        System.setProperty("webdriver.gecko.driver","/Users/kelbenj/Downloads/geckodriver");
+        this.driver = new FirefoxDriver();
+        this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        this.driver.get("http://localhost:8080/");
+        this.driver.findElement(By.id("username")).sendKeys("Ronald");
+        this.driver.findElement(By.className("act-but")).click();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.open(\"http://localhost:8080\");");
+        ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        this.driver.findElement(By.id("username")).sendKeys("Sam");
+        this.driver.findElement(By.className("act-but")).click();
+        assertEquals("2",driver.findElement(By.className("chat-num")).getText());
+        this.driver.findElement(By.id("exitButton")).click();
+        driver.switchTo().window(tabs.get(0));
+        assertEquals("1",driver.findElement(By.className("chat-num")).getText());
+        this.driver.quit();
     }
 }
